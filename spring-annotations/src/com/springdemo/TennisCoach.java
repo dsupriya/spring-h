ee5550b84b8f4dbe25196023a818e3988a72c380
Supriya B. Dhavalshankh <supriyad570@gmail.com>
2021-03-25 21:58:59 +0530

    AnnotationScopeDemo

--- spring-annotations/src/com/springdemo/TennisCoach.java
+++ spring-annotations/src/com/springdemo/TennisCoach.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+//Application Context
 @Component
 @Scope("prototype")
 public class TennisCoach implements Coach {
